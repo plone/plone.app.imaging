33aee4a5ce1279ec49a2d3f6ab3fd30b0f5318f7
Andreas Zeidler <witsch@plone.org>
2008-08-07 23:22:05 +0000

    clean up package metadata
    svn path=/plone.app.imaging/trunk/; revision=22038

--- README.txt
+++ README.txt
@@ -1,4 +1,13 @@
-Introduction
-============
+plone.app.imaging
+=================
 
+Introduction
+------------
+
+This package tries to factor out and re-use the image scaling code from
+Archetypes_ into a separate package in order to make it user-configurable
+and add support for storing the image data into ZODB blobs_.
+
+  .. _Archetypes: http://plone.org/products/archetypes
+  .. _blobs: http://plone.org/products/plone.app.blob
 
--- setup.py
+++ setup.py
@@ -1,33 +1,38 @@
 from setuptools import setup, find_packages
-import os
+from os.path import join
 
-version = '1.0'
+name = 'plone.app.imaging'
+path = name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
+readme = open("README.txt").read()
+history = open(join('docs', 'HISTORY.txt')).read().replace(name + ' - ', '')
 
-setup(name='plone.app.imaging',
-      version=version,
-      description="User-configurable, blob-aware image scaling for Plone",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='images scaling blob plone',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://svn.plone.org/svn/plone/plone.app.imaging/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
+setup(name = name,
+      version = version,
+      description = 'User-configurable, blob-aware image scaling for Plone.',
+      long_description = readme[readme.find('\n\n'):] + '\n' + history,
+      keywords = 'images scaling zodb blob plone',
+      author = 'Andreas Zeidler - Plone Foundation',
+      author_email = 'plone-developers@lists.sourceforge.net',
+      url = 'http://svn.plone.org/svn/plone/plone.app.imaging/',
+      license = 'GPL',
+      packages = find_packages(),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data = True,
+      install_requires = ['setuptools',],
+      platforms = 'Any',
+      zip_safe = False,
+      classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+)
+
