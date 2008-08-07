bbf8b9cc04924be0f2c8db3468802b083009fd62
Andreas Zeidler <witsch@plone.org>
2008-08-07 23:25:10 +0000

    simplify the adapter code and make it more readable (imho)
    svn path=/plone.app.imaging/trunk/; revision=22047

--- plone/app/imaging/traverse.py
+++ plone/app/imaging/traverse.py
@@ -4,40 +4,26 @@ from Products.Archetypes.interfaces import IBaseObject
 from Products.Archetypes.atapi import ImageField
 from ZPublisher.BaseRequest import DefaultPublishTraverse
 
-class Fallback(Exception): pass
 
 class ImageTraverser(DefaultPublishTraverse):
+    """ traversal adapter for scaled down versions of image content """
     adapts(IBaseObject, IRequest)
 
     def fallback(self, request, name):
         return super(ImageTraverser, self).publishTraverse(request, name)
 
-
     def publishTraverse(self, request, name):
-        schema=self.context.Schema()
-
-        if "_" in name:
-            (fieldname, scale)=name.split("_", 1)
+        schema = self.context.Schema()
+        if '_' in name:
+            fieldname, scale = name.split('_', 1)
         else:
-            (fieldname, scale)=(name, None)
-
-        try:
-            field=schema.get(fieldname)
-            if field is None:
-                raise Fallback
-
-            if not isinstance(field, ImageField):
-                raise Fallback
-
-            if scale is not None and scale not in field.getAvailableSizes(self.context):
-                raise Fallback
-
-            image=field.getScale(self.context, scale=scale)
-            if image is not None and not isinstance(image, basestring):
-               return image
-        except Fallback:
-            pass
-
+            fieldname, scale = name, None
+        field = schema.get(fieldname)
+        if field is not None and isinstance(field, ImageField):
+            available = field.getAvailableSizes(self.context)
+            if scale in available or scale is None:
+                image = field.getScale(self.context, scale=scale)
+                if image is not None and not isinstance(image, basestring):
+                   return image
         return self.fallback(request, name)
-
 
