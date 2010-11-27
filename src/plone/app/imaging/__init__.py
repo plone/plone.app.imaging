224d77c69d9b3a760224a4b099592ba6a4c077b8
David Glick <davidglick@groundwire.org>
2010-11-27 04:58:19 +0000

    protect the control panel with a more specific permission
    svn path=/plone.app.imaging/trunk/; revision=45978

--- src/plone/app/imaging/__init__.py
+++ src/plone/app/imaging/__init__.py
@@ -1,6 +1,8 @@
+from Products.CMFCore.permissions import setDefaultRoles
 from plone.app.imaging.monkey import patchImageField
 from plone.app.imaging.monkey import patchSchemas
 
+setDefaultRoles('Plone Site Setup: Imaging', ('Manager', 'Site Administrator'))
 
 def initialize(context):
     patchImageField()       # patch ImageField's `getAvailableSizes` method
