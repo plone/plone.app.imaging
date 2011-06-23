244d24108bd1313c244feff32ae71721fe80c833
Yiorgis Gozadinos <ggozad@jarn.com>
2011-06-23 07:39:29 +0000

    Use overlays for the cropping of a single image
    svn path=/plone.app.imaging/branches/ggozad-cropping/; revision=50640

--- src/plone/app/imaging/resources/cropping.js
+++ src/plone/app/imaging/resources/cropping.js
@@ -1,40 +1,47 @@
+jQuery(document).ready(function () {
+    jQuery('.selImageToCropBtn').prepOverlay({
+         subtype: 'ajax',
+         config: {
+             onLoad: function () {
+                 var cropbox = null;
+                 if (jQuery('img#croppableImage').length > 0) {
+                     var field = jQuery('img#croppableImage').attr('data-field');
+                     var scale = jQuery('img#croppableImage').attr('data-scale');
+                     var ar = jQuery('img#croppableImage').attr('data-aspectratio');
+                     var jcrop = jQuery.Jcrop('img#croppableImage');
+                     jcrop.setOptions({aspectRatio: ar,
+                                       allowSelect: true,
+                                       allowResize: true,
+                                       allowMove: true,
+                                       onSelect: function (coords) {
+                                           jQuery('input#image-recrop').removeAttr('disabled');
+                                           cropbox = coords;
+                                       }
+                      });
+                     jcrop.focus();
 
-$(window).load(function () {
-    var cropbox = null;
-    if ($('img#croppableImage').length > 0) {
-        var field = $('img#croppableImage').attr('data-field');
-        var scale = $('img#croppableImage').attr('data-scale');
-        var ar = $('img#croppableImage').attr('data-aspectratio');
-        var jcrop = $.Jcrop('img#croppableImage');
-        jcrop.setOptions({aspectRatio: ar,
-                          allowSelect: true,
-                          allowResize: true,
-                          allowMove: true,
-                          onSelect: function (coords) {
-                              $('input#image-recrop').removeAttr('disabled');
-                              cropbox = coords;
-                          }
-         });
-        jcrop.focus();
+                     jQuery('input#image-recrop').click(function (e) {
+                         e.preventDefault();
+                         var context_url = jQuery('base').attr('href');
+                         jQuery.ajax({
+                            type: "GET",
+                            url: context_url+'/@@cropimage/cropImage',
+                            data: {field: field,
+                                   scale: scale,
+                                   x1: cropbox.x,
+                                   y1: cropbox.y,
+                                   x2: cropbox.x2,
+                                   y2: cropbox.y2
+                                  },
+                            success: function () {
+                                window.location.replace(context_url+'/cropping');
+                            }
+                         });
+                     });
+                 }
 
-        $('input#image-recrop').click(function (e) {
-            e.preventDefault();
-            var context_url = $('base').attr('href');
-            $.ajax({
-               type: "GET",
-               url: context_url+'/@@cropimage/cropImage',
-               data: {field: field,
-                      scale: scale,
-                      x1: cropbox.x,
-                      y1: cropbox.y,
-                      x2: cropbox.x2,
-                      y2: cropbox.y2
-                     },
-               success: function () {
-                   window.location.replace(context_url+'/cropping');
-               }
-            });
-        });
-    }
+
+             }
+         },
+     });
 });
-
