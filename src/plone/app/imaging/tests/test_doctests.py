51165c2f5799de591517dd72aa76eb8e48ec46dc
Tom Gross <itconsense@gmail.com>
2015-01-19 16:10:28 +0100

    (re)added imaging doctests

--- src/plone/app/imaging/tests/test_doctests.py
+++ src/plone/app/imaging/tests/test_doctests.py
@@ -9,7 +9,6 @@ optionflags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
 
 def test_suite():
     suite = TestSuite()
-    return suite
     for testfile in ['traversal.txt', 'transforms.txt', 'configlet.txt']:
         suite.addTest(layered(doctest.DocFileSuite(testfile,
                                            package='plone.app.imaging.tests'),
